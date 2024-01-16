c9f4341005d5a55c18ee356f3ab0b9f31d54f7c4
Zain-Irfan009 <zain.irfan@tetralogicx.com>
2024-01-16 14:06:25 +0500

    commit

---
+++ database/migrations/2024_01_16_075225_create_webhook_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webhook_logs', function (Blueprint $table) {
+            $table->id();
+            $table->longText('log')->nullable();
+            $table->string('status')->nullable();
+            $table->longText('failed_reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webhook_logs');
+    }
+};
